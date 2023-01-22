5a3ecfc71dc04a04eeb80a026f1713bf948e2aaf
Yuliia Ermakova <57410256+julia992@users.noreply.github.com>
2023-01-22 14:18:55 +0100

    Add files via upload

---
+++ city.country.py
@@ -0,0 +1,10 @@
+def city_country(city, country):
+    """ return name city and country."""
+    name =  city + '  ' + country
+    return  name.title() 
+sum = city_country('santiago' , 'chilie')
+print(sum)
+sma = city_country('poznan' , 'poland')
+print(sma)
+
+    
