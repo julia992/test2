ab6325043722d953ce29af91db8b8d762fa18cf3
Yuliia Ermakova <57410256+julia992@users.noreply.github.com>
2023-02-11 10:22:34 +0100

    project Alien game

---
+++ alien_invasion.py
@@ -0,0 +1,34 @@
+import sys
+
+import pygame
+
+from settings import Settings
+
+def run_game():
+#Initilizian game and create object of display.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+    pygame.display.set_caption("Alien Invasion")
+
+#Choose main color background.
+bg_color = (230, 230, 230)
+
+#Play tne main range of game.
+while True:
+    
+#Monitor display and mouse events.
+         for event in pygame.event.get():
+             if event.type == pygame.QUIT:
+                 sys.exit()
+
+#Screen is drawn with each transition of the loop.
+screen.fill(ai_settings.bg_color)
+
+
+#Display last drawn screen.
+pygame.display.flip()
+         
+run_game()
+
