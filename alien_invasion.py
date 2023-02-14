f232dd760d404bdc851b25fc4f1cae4e4111224d
Yuliia Ermakova <57410256+julia992@users.noreply.github.com>
2023-02-14 14:45:46 +0100

    true

--- alien_invasion.py
+++ alien_invasion.py
@@ -20,7 +20,7 @@ def run_game():
 
 #Start tne main range of game.
     while True:
-        gf.check_events()
+        gf.check_events(ship)
         gf.update_screen(ai_settings, screen, ship)
     
 #Monitor display and mouse events.
