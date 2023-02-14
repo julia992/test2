7cf6942bfb6f17e24638de843581d462131ab745
Yuliia Ermakova <57410256+julia992@users.noreply.github.com>
2023-02-14 11:08:29 +0100

    Add files via upload

--- game_functions.py
+++ game_functions.py
@@ -7,3 +7,12 @@ def check_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+def update_screen(ai_settings, screen, ship):
+    """Refreshes the screen image and displays the new screen."""
+    # The screen is redrawn on each iteration of the loop.
+    screen.fill(ai_settings.bg_color)
+    ship.blitme()
+
+    # Display the last rendered screen.
+    pygame.display.flip()
