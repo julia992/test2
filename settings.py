22f9cd498fa3e2fb5dd869d94c5a3630d39c31bb
Yuliia Ermakova <57410256+julia992@users.noreply.github.com>
2023-02-16 15:20:11 +0100

    Delete settings.py

--- settings.py
+++
@@ -1,9 +0,0 @@
-class Settings():
-    """Class for save all options for game Alien Invasion."""
-
-    def __init__(self):
-        """Initilization options game."""
-        #Settings screen
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 230, 230)
